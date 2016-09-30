5462d4b41846074f31b30a339cc393b7c1e4b2e1
pcraddock <pcraddock360@gmail.com>
2016-09-30 14:12:53 +0100

    Add files via upload

---
+++ main/Drag.py
@@ -0,0 +1,15 @@
+import level1
+
+def Drag(density, velocity, dragCoeff, Area):
+	# F = force due to drag
+	p = density
+	v = velocity
+	c = dragCoeff
+	A = Area
+	if v>0:
+		F = 0.5*p*(v**2)*c*A
+	elif v<0:
+		F = -0.5*p*(v**2)*c*A
+	else:
+		F=0
+	return F
